12bb0d2bfddc8c560ef7ccb888a5dc2d4297172c
ITSCrew-Keith <seaglethebeagle@gmail.com>
2025-01-30 21:26:03 -0600

    new game 2
    new game 2

---
+++ side-project-home-version-0.1/scripts/enemy_hitbox.gd
@@ -0,0 +1,11 @@
+extends Area2D
+
+func _on_body_entered(body: Node2D) -> void:
+	if body.is_in_group("player"):
+		get_tree().reload_current_scene()
+		Global.timer = 0
+		Global.jump_counter = 120
+		Global.jump_counter2 = 25
+		Global.coin_counter = 450
+		Global.e_speed = 0
+		Global.enemy_counter = 100
