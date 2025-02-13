3ced258a021bd24a2b67f18177c2d9542bd3cab9
ITSCrew-Keith <seaglethebeagle@gmail.com>
2025-02-13 14:43:20 -0600

    energy lvl bar update
    energy lvl bar update

---
+++ its-crew-keith-geometry-game-main-side-project-home-version-0.1-3/scripts/dasher.gd
@@ -0,0 +1,20 @@
+extends CharacterBody2D
+
+
+@onready var target= $"../player"
+
+
+
+func _physics_process(delta: float) -> void:
+	var speed = 1000 + (Global.timer / 10) + (Global.e_speed)
+	var direction = (target.position-position).normalized()
+	velocity = direction * speed
+	if Global.jump_counter > 0:
+		Global.jump_counter = Global.jump_counter - 1
+	if Global.jump_counter == 0:
+		move_and_slide()
+		Global.jump_counter2 = Global.jump_counter2 - 1
+		if Global.jump_counter2 == 0:
+			Global.jump_counter = 300
+			Global.jump_counter2 = 50
+	
