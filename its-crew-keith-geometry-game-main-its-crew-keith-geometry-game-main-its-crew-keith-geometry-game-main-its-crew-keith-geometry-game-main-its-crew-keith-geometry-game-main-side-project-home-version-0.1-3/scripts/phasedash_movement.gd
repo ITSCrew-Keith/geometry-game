a3a5183b29d55e3ad3ee3921c20741006fca9f9e
ITSCrew-Keith <seaglethebeagle@gmail.com>
2025-02-20 12:05:09 -0600

    blue update
    blue update

---
+++ its-crew-keith-geometry-game-main-its-crew-keith-geometry-game-main-its-crew-keith-geometry-game-main-its-crew-keith-geometry-game-main-its-crew-keith-geometry-game-main-side-project-home-version-0.1-3/scripts/phasedash_movement.gd
@@ -0,0 +1,23 @@
+extends CharacterBody2D
+var phase = 1
+var phase_swap = false
+
+func _physics_process(delta: float) -> void:
+	var target = Vector2(Global.playerx,Global.playery)
+	var speed = 1500
+	var direction = (target - position).normalized()
+	velocity = direction * speed
+	
+	if Global.jump_counter == 0:
+		move_and_slide()
+		
+		if phase_swap == false:
+			if phase == 1:
+				phase = 0
+				phase_swap = true
+			else:
+				phase = 1
+				phase_swap = true
+				
+	if Global.jump_counter > 0 and phase_swap == true:
+		phase_swap = false
