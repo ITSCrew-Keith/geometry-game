3ed957d730889c9de641607fabce04695ee4a73b
ITSCrew-Keith <seaglethebeagle@gmail.com>
2025-02-21 15:00:32 -0600

    Delete its-crew-keith-geometry-game-main-side-project-home-version-0.1-3 directory

--- its-crew-keith-geometry-game-main-side-project-home-version-0.1-3/scripts/powerup.gd
+++
@@ -1,9 +0,0 @@
-extends Area2D
-signal coin_collection
-
-func _on_body_entered(body: Node2D) -> void:
-	if Global.laser_energy < 500:
-		Global.laser_energy += 50
-	Global.e_speed = Global.e_speed - 30
-	queue_free()
-	emit_signal("coin_collection")
