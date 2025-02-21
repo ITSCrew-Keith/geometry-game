272cea654bdf100fba6930c025a4ddf282939df4
ITSCrew-Keith <seaglethebeagle@gmail.com>
2025-02-21 14:44:38 -0600

    boss update
    boss update

--- its-crew-keith-geometry-game-main-orange-void/scripts/Global.gd
+++ its-crew-keith-geometry-game-main-orange-void/scripts/Global.gd
@@ -11,6 +11,7 @@ var laser_energy = 50
 var dash_energy = 250
 var best_time = 0
 var boss_counter = 300
+var health = 10
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
--- its-crew-keith-geometry-game-main-orange-void/scripts/boss_hitbox.gd
+++ its-crew-keith-geometry-game-main-orange-void/scripts/boss_hitbox.gd
@@ -2,14 +2,15 @@ extends Area2D
 @onready var boss_mini: PackedScene = preload("res://scenes/boss.tscn")
 @onready var boss = $".."
 
-
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("laser"):
+		Global.health = Global.health - 1
 		for i in range(2):
-			var instance = boss_mini.instantiate()
-			instance.position = boss.global_position + Vector2(randi_range(-300, 300), randi_range(-300, 300))
-			get_tree().current_scene.add_child(instance)
-			get_parent().queue_free()
+			if Global.health > 0:
+				var instance = boss_mini.instantiate()
+				instance.position = boss.global_position + Vector2(randi_range(-1000, 1000),randi_range(-1000, 1000))
+				get_tree().current_scene.add_child(instance)
+		get_parent().queue_free()
 	if body.is_in_group("player"):
 		if Global.timer > Global.best_time:
 			Global.best_time = Global.timer
